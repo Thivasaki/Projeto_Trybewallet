5838eae5d83f07f808b2dfde79dc74c196bf21c4
thiago-ivasaki <thiagoivasaki@gmail.com>
2022-08-29 16:59:51 -0300

    Iniciado projeto

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,5 @@
 // import user from './user';
-// import wallet from './wallet';
+// import wallet from './wallet'; oi
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
