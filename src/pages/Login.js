e690efd79d23fa2afdeea4b5e13ece5b127bc309
thiago-ivasaki <thiagoivasaki@gmail.com>
2022-09-01 15:36:00 -0300

    Finalizado requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,81 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addLoginAction } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      disableButton: false,
+    };
+  }
+
+  validation = () => {
+    const { password, email } = this.state;
+    const minNumbPassword = 6;
+    const emailValidation = email.match(/@email.com/i) !== null;
+    const Validation = password.length >= minNumbPassword
+    && emailValidation;
+    this.setState({ disableButton: Validation });
+  };
+
+  handleValue = ({ target }) => {
+    const { value } = target;
+    const { id } = target;
+    this.setState({ [id]: value }, this.validation);
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { email } = this.state;
+    const { dispatch, history } = this.props;
+    dispatch(addLoginAction(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { disableButton } = this.state;
+    return (
+      <form>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            id="email"
+            data-testid="email-input"
+            onChange={ (event) => this.handleValue(event) }
+          />
+        </label>
+        <label htmlFor="password">
+          Senha
+          <input
+            type="password"
+            id="password"
+            data-testid="password-input"
+            onChange={ (event) => this.handleValue(event) }
+          />
+        </label>
+        <button
+          type="submit"
+          disabled={ !disableButton }
+          onClick={ this.handleSubmit }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,8 @@
-// Coloque aqui suas actions
+export const ADD_LOGIN = 'ADD_LOGIN';
+
+export function addLoginAction(payload) {
+  return {
+    type: ADD_LOGIN,
+    payload,
+  };
+}
